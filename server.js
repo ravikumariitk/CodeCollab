d76b3c2a083ac6beb46a21a5235f39f420c1ccec
Ravi <103475192+ravikumariitk@users.noreply.github.com>
2025-08-06 22:33:09 +0530

    CORS error

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const { WebsocketProvider } = require('y-websocket');
 const ydoc = new Y.Doc();
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
 
 app.use(express.static('build'));
 app.use((req, res, next) => {
